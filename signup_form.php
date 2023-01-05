6611d4ddd098bc7771a153102cfb356a3d1d56ac
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-05 14:46:50 +0100

    add bootstrap

--- signup_form.php
+++ signup_form.php
@@ -8,6 +8,8 @@ require_once 'classes/Connexion.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8b26d30613.js" crossorigin="anonymous"></script>
     <title>S'inscrire - Memory</title>
 </head>
 <body>
@@ -19,7 +21,7 @@ require_once 'classes/Connexion.php';
             <section>
                 <div id="form_up">
                     <form action="" method="post">
-                        
+
                     </form>
                 </div>
             </section>
