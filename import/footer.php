d89671bd6e2391f0d2a9762c7ff07f722839b7f9
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-06 16:26:00 +0100

    footer style

---
+++ import/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer p-3 mt-4 text-center bg-dark text-light">
+                    <p>© 2023 - Tous droits réservés - <a href="https://github.com/jules-jean-louis1/memory" target="_blank">Jules Jean-Louis</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
