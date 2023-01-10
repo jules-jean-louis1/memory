70183abe7ae7c0b87a5bfb09a597ead2d21840f0
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-10 14:50:32 +0100

    ajout fichier config

---
+++ import/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define("DB_HOST", 'localhost');
+define("DB_NAME", 'memory');
+define('DB_CHARSET', 'utf8');
+define("DB_USER", 'root');
+define("DB_PASSWORD", '');
+
+// Plesk config; use this instead of the above
+// define("DB_HOST", 'localhost');
+// define("DB_NAME", 'jules_jean-louis_memory');
+// define('DB_CHARSET', 'utf8');
+// define(DB_USER", 'memory_game');
+// define("DB_PASSWORD", 'memory_game');
