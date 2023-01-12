ac2f75b3e088a4664a71118dccfe5610f166accd
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-12 10:06:24 +0100

    add image

--- import/Creation_card.php
+++ import/Creation_card.php
@@ -1,34 +1,81 @@
 <?php
-$lucifer = new Image ("images/cartes/01_as_de_pique.svg", "images/cartes/backofcards.png", 1, 1);
-$reading = new Image ("images/cartes/01_as_de_pique.svg", "images/cartes/backofcards.png", 2, 1);
-$renaissance = new Image ("images/cartes/208px-06_of_spades.svg.png", "images/cartes/backofcards.png", 3, 1);
-$spring1 = new Image ("images/cartes/208px-06_of_spades.svg.png", "images/cartes/backofcards.png", 4, 1);
-$spring2 = new Image ("images/cartes/07_of_diamonds.svg", "images/cartes/backofcards.png", 5, 1);
-$spring3 = new Image ("images/cartes/07_of_diamonds.svg", "images/cartes/backofcards.png", 6, 1);
-$luciferBis = new Image ("images/cartes/02_deux_de_pique.png", "images/cartes/backofcards.png", 7, 1);
-$readingBis = new Image ("images/cartes/02_deux_de_pique.png", "images/cartes/backofcards.png", 8, 1);
-$renaissanceBis = new Image ("images/cartes/03_trois_de_pique.png", "images/cartes/backofcards.png", 9, 1);
-$spring1Bis = new Image ("images/cartes/03_trois_de_pique.png", "images/cartes/backofcards.png", 10, 1);
-$spring2Bis = new Image ("Images/spring2.png", "images/cartes/backofcards.png", 11, 1);
-$spring3Bis = new Image ("Images/spring3.png", "images/cartes/backofcards.png", 12, 1);
-$crown = new Image ("Images/crown.png", "images/cartes/backofcards.png", 13, 1);
-$renaissance1 = new Image ("Images/renaissance1.png", "images/cartes/backofcards.png", 14, 1);
-$eyes = new Image ("Images/eyes.png", "images/cartes/backofcards.png", 15, 1);
-$sunflowers = new Image ("Images/sunflowers.png", "images/cartes/backofcards.png", 16, 1);
-$grec = new Image ("Images/grec.png", "images/cartes/backofcards.png", 17, 1);
-$laurier = new Image ("Images/laurier.png", "images/cartes/backofcards.png", 18, 1);
-$crownBis = new Image ("Images/crown.png", "images/cartes/backofcards.png", 19, 1);
-$renaissance1Bis = new Image ("Images/renaissance1.png", "images/cartes/backofcards.png", 20, 1);
-$eyesBis = new Image ("Images/eyes.png", "images/cartes/backofcards.png", 21, 1);
-$sunflowersBis = new Image ("Images/sunflowers.png", "images/cartes/backofcards.png", 22, 1);
-$grecBis = new Image ("Images/grec.png", "images/cartes/backofcards.png", 23, 1);
-$laurierBis = new Image ("Images/laurier.png", "images/cartes/backofcards.png", 24, 1);
+
+$carte1 = new Image ("images/cartes/img01.png", "images/cartes/backofcards.png", 1, 1);
+$carte2 = new Image ("images/cartes/img02.png", "images/cartes/backofcards.png", 2, 1);
+$carte3 = new Image ("images/cartes/img03.png", "images/cartes/backofcards.png", 3, 1);
+$carte4 = new Image ("images/cartes/img04.png", "images/cartes/backofcards.png", 4, 1);
+$carte5 = new Image ("images/cartes/img05.png", "images/cartes/backofcards.png", 5, 1);
+$carte6 = new Image ("images/cartes/img06.png", "images/cartes/backofcards.png", 6, 1);
+$carte7 = new Image ("images/cartes/img07.png", "images/cartes/backofcards.png", 7, 1);
+$carte8 = new Image ("images/cartes/img08.png", "images/cartes/backofcards.png", 8, 1);
+$carte9 = new Image ("images/cartes/img09.png", "images/cartes/backofcards.png", 9, 1);
+$carte10 = new Image ("images/cartes/img10.png", "images/cartes/backofcards.png", 10, 1);
+$carte11 = new Image ("images/cartes/img11.png", "images/cartes/backofcards.png", 11, 1);
+$carte12 = new Image ("images/cartes/img12.png", "images/cartes/backofcards.png", 12, 1);
+$carte13 = new Image ("images/cartes/img13.png", "images/cartes/backofcards.png", 13, 1);
+$carte14 = new Image ("images/cartes/img14.png", "images/cartes/backofcards.png", 14, 1);
+$carte15 = new Image ("images/cartes/img15.png", "images/cartes/backofcards.png", 15, 1);
+$carte16 = new Image ("images/cartes/img16.png", "images/cartes/backofcards.png", 16, 1);
+$carte17 = new Image ("images/cartes/img17.png", "images/cartes/backofcards.png", 17, 1);
+$carte18 = new Image ("images/cartes/img18.png", "images/cartes/backofcards.png", 18, 1);
+$carte19 = new Image ("images/cartes/img19.png", "images/cartes/backofcards.png", 19, 1);
+$carte20 = new Image ("images/cartes/img20.png", "images/cartes/backofcards.png", 20, 1);
+$carte21 = new Image ("images/cartes/img21.png", "images/cartes/backofcards.png", 21, 1);
+$carte22 = new Image ("images/cartes/img22.png", "images/cartes/backofcards.png", 22, 1);
+$carte23 = new Image ("images/cartes/img23.png", "images/cartes/backofcards.png", 23, 1);
+$carte24 = new Image ("images/cartes/img24.png", "images/cartes/backofcards.png", 24, 1);
 
 
-$faceUpArrayEasy = array($lucifer, $reading, $renaissance, $luciferBis, $readingBis, $renaissanceBis);
-$faceUpArrayMedium = array($lucifer, $reading, $renaissance, $spring1, $spring2,
-$spring3, $luciferBis, $readingBis, $renaissanceBis, $spring1Bis, $spring2Bis, $spring3Bis);
-$faceUpArrayHard = array($lucifer, $reading, $renaissance, $spring1, $spring2,
-$spring3, $crown, $renaissance1, $eyes, $sunflowers, $grec, $laurier, $luciferBis, $readingBis, $renaissanceBis, $spring1Bis, $spring2Bis, $spring3Bis, $crownBis, $renaissance1Bis, $eyesBis, $sunflowersBis, $grecBis, $laurierBis);
+$faceUpArrayEasy = 
+[
+    $carte1, 
+    $carte2, 
+    $carte3, 
+    $carte4, 
+    $carte5, 
+    $carte6
+];
+$faceUpArrayMedium = 
+[
+    $carte1,
+    $carte2, 
+    $carte3, 
+    $carte4, 
+    $carte5,
+    $carte6, 
+    $carte7, 
+    $carte8, 
+    $carte9, 
+    $carte10, 
+    $carte11, 
+    $carte12
+];
+$faceUpArrayHard = 
+[
+    $carte1,
+    $carte2, 
+    $carte3, 
+    $carte4, 
+    $carte5,
+    $carte6, 
+    $carte7, 
+    $carte8, 
+    $carte9, 
+    $carte10, 
+    $carte11, 
+    $carte12,
+    $carte13,
+    $carte14,
+    $carte15,
+    $carte16,
+    $carte17,
+    $carte18,
+    $carte19,
+    $carte20,
+    $carte21,
+    $carte22,
+    $carte23,
+    $carte24,
+];
 
 ?>
