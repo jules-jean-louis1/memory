801ade978ea332295f7017ca9c13872528fb2638
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-12 14:01:00 +0100

    background

--- test.php
+++ test.php
@@ -1,10 +1,8 @@
 <?php 
-$id = 'jules';
-$pdo = new PDO('mysql:host=localhost;dbname=memory', 'root', '');
-$sql = "SELECT * FROM utilisateurs WHERE username = :username";
-$query = $pdo->prepare($sql);
-$query->bindParam(':username', $id);
-$query->execute();
-$user = $query->fetch();
-var_dump($user);
-echo $user['username'];
+
+$cards =
+[
+    ["nom" => "as de piques", "image" => "img01.png", "paire" => 1],
+    ["nom" => "as de piques", "image" => "img01.png", "paire" => 1],
+    ["nom" => "as de coeurs", "image" => "img02.png", "paire" => 2],
+]
