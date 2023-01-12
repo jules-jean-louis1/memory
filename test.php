9da5f9ab94d467a23ec60489f88ab61fa299c746
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-12 14:32:03 +0100

    delete

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$cards =
-[
-    ["nom" => "as de piques", "image" => "img01.png", "paire" => 1],
-    ["nom" => "as de piques", "image" => "img01.png", "paire" => 1],
-    ["nom" => "as de coeurs", "image" => "img02.png", "paire" => 2],
-]
